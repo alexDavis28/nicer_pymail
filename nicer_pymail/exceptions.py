342a0f613a671b24c83ec6724a065154e531f525
Alex Davis <8m3xlt@gmail.com>
2020-08-05 00:31:44 +0100

    added AttachmentNotFoundException

--- nicer_pymail/exceptions.py
+++ nicer_pymail/exceptions.py
@@ -6,7 +6,19 @@ class ClientException(NicerPyMailException):
     pass
 
 
-class AuthenticationError(ClientException):
+class EmailException(NicerPyMailException):
+    pass
+
+
+class AuthenticationException(ClientException):
     def __str__(self):
         return "Authentication failed, check that your username and password are correct, and that your account " \
                "allows less secure applications."
+
+
+class AttachmentNotFoundException(EmailException):
+    def __init__(self, file: str):
+        self.file = file
+
+    def __str__(self):
+        return f"This file wasn't found: {self.file}"
