fd1512c70641d40879ad99838e513290c02715ee
Alex Davis <8m3xlt@gmail.com>
2020-08-06 14:38:09 +0100

    added field for incoming server, and changed how contexts are used

--- nicer_pymail/client.py
+++ nicer_pymail/client.py
@@ -10,16 +10,19 @@ from .email import Email
 
 
 class Client:
-    def __init__(self, email_address: str, password: str, host: str = "smtp.gmail.com", port: int = 465,
+    def __init__(self, email_address: str, password: str, outgoing_server: str = "smtp.gmail.com",
+                 incoming_server: str = "imap.gmail.com", port: int = 465,
                  do_print: bool = False):
+        self.incoming_server = incoming_server
         self.email_address = email_address
         self.password = password
-        self.host = host
+        self.outgoing_server = outgoing_server
         self.port = port
-        self.context = ssl.create_default_context()
         self.do_print = do_print
 
-        with smtplib.SMTP_SSL(self.host, self.port, context=self.context) as server:
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(self.outgoing_server, self.port, context=context) as server:
             try:
                 server.login(self.email_address, self.password)
                 if do_print:
@@ -29,7 +32,10 @@ class Client:
 
     def send_plaintext_email(self, recipient_email: str, msg: str):
         """Send a simple plaintext email to the specified address"""
-        with smtplib.SMTP_SSL(self.host, self.port, context=self.context) as server:
+
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(self.outgoing_server, self.port, context=context) as server:
             try:
                 server.login(self.email_address, self.password)
             except smtplib.SMTPAuthenticationError:
@@ -85,7 +91,9 @@ class Client:
                 except FileNotFoundError:
                     raise AttachmentNotFoundException(file)
 
-        with smtplib.SMTP_SSL(self.host, self.port, context=self.context) as server:
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL(self.outgoing_server, self.port, context=context) as server:
             try:
                 server.login(self.email_address, self.password)
             except smtplib.SMTPAuthenticationError:
