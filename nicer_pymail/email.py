8eb9e5cb342a1cd3422737b90baf2d1d6267e47c
Alex Davis <8m3xlt@gmail.com>
2020-08-06 17:01:59 +0100

    added get_emails

--- nicer_pymail/email.py
+++ nicer_pymail/email.py
@@ -1,15 +1,13 @@
 class Email:
-    def __init__(self, subject: str = "None", plaintext: str = "None", cc: list = [], bcc: list = [],
+    def __init__(self, subject: str = "None", plaintext: str = "None",
                  attachments: list = [],
                  from_address: str = None,
-                 to_address: str = None, html: str = None):
+                 recipients: str = None, html: str = None):
         self.subject = subject
         self.plaintext = plaintext
-        self.bcc = bcc
-        self.cc = cc
         self.attachments = attachments
         self.from_address = from_address
-        self.to_address = to_address
+        self.recipients = recipients
         self.html = html
 
     def add_attachment(self, path_to_file: str):
